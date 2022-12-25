38e64afb2327542cf0c6867e5d31b5779af58963
Danya Kovan <mr.kovan_2002@mail.ru>
2022-12-25 22:44:19 +0100

    +inactive obtained-button state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,45 +1,47 @@
 class Card {
-  constructor (product, amount, selector, transferCard) {
+  constructor(product, amount, selector, transferCard) {
     this._productName = product
     this._productAmount = amount
     this._selector = selector
     this._transferCard = transferCard
   }
 
-_getTemplate () {
-  const cardElement = document
-  .querySelector(this._selector)
-  .content
-  .querySelector('.container__card')
-  .cloneNode(true)
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._selector)
+      .content
+      .querySelector('.container__card')
+      .cloneNode(true)
 
-  return cardElement
-}
+    return cardElement
+  }
 
-generateCard () {
-  this._card = this._getTemplate()
-  this._product = this._card.querySelector('.container__card-product')
-  this._amount = this._card.querySelector('.container__card-amount')
+  generateCard() {
+    this._card = this._getTemplate()
+    this._product = this._card.querySelector('.container__card-product')
+    this._amount = this._card.querySelector('.container__card-amount')
 
-  this._product.textContent = this._productName
-  this._amount.textContent = this._productAmount
+    this._product.textContent = this._productName
+    this._amount.textContent = this._productAmount
 
-this._setEventListeners(this._card)
+    this._setEventListeners(this._card)
 
-  return this._card
-}
+    return this._card
+  }
 
-_obtainedProduct (card) {
-  card.classList.add('container__card_type_obtained')
-  this._transferCard(card)
-}
+  _obtainedProduct(card) {
+    card.classList.add('container__card_type_obtained')
+    card.querySelector('.container__card-amount').classList.add('container__card-amount_type_obtained')
+    card.querySelector('.container__obtained-button').classList.add('container__obtained-button_inactive')
+    this._transferCard(card)
+  }
 
-_setEventListeners (card) {
-  card.querySelector('.container__obtained-button')
-  .addEventListener('click', () => {
-    this._obtainedProduct(card)
-  })
-}
+  _setEventListeners(card) {
+    card.querySelector('.container__obtained-button')
+      .addEventListener('click', () => {
+        this._obtainedProduct(card)
+      })
+  }
 
 
 }
